f2a8503d534f8d8af08d6472335828608dafa689
Virginie Demeyer <dmy.virginie@gmail.com>
2022-10-18 19:11:35 +0100

    Ajout de jsonwebtoken pour la création de token d'authentification et création du middleware d'authentification

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 mongoose.connect('mongodb+srv://VirginieD:ForTest@cluster0.mdkm33h.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
