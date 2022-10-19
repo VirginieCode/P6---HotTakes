d93e00e2ae7ecaf9dce111ce9f6c7aecdd529735
Virginie Demeyer <dmy.virginie@gmail.com>
2022-10-19 17:08:49 +0100

    Ajout de multer

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 
+const path = require('path');
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -37,5 +39,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-const stuffCtrl = require('../controllers/stuff');
+const stuffCtrl = require('../controllers/sauces');
 
-router.get('/', auth, stuffCtrl.getAllThings);
-router.post('/', auth, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllSauces);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.get('/:id', auth, stuffCtrl.getOneSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
+router.delete('/:id', auth, stuffCtrl.deleteSauce);
 
 module.exports = router;
